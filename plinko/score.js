1f8b12013ee7b8ca019856dda93752735b2bbb99
Miguel Bolivar <MBolivar360@gmail.com>
2019-01-31 15:36:14 -0400

    Adding knn for n features and assuring distance function to work with arrays

--- plinko/score.js
+++ plinko/score.js
@@ -1,7 +1,12 @@
 const outputs = [];
 
-function distance(point, predictionPoint) {
-  return Math.abs(point - predictionPoint);
+function distance(pointA, pointB) {
+  // point A and B would be arrays
+  return _.chain(pointA)
+    .zip(pointB)
+    .map(([a, b]) => (a-b) ** 2)
+    .sum()
+    .value() ** 0.5
 }
 
 function onScoreUpdate(dropPosition, bounciness, size, bucketLabel) {
@@ -9,12 +14,12 @@ function onScoreUpdate(dropPosition, bounciness, size, bucketLabel) {
 }
 
 function runAnalysis() {
-  const testSetSize = 100;
+  const testSetSize = 10;
 
     _.range(1, 15).forEach(k => {
       const [testSet, trainingSet] = splitDataset(outputs, testSetSize, k);
       const accuracy = _.chain(testSet)
-        .filter(testPoint => knn(trainingSet, testPoint[0]) === testPoint[3] )
+        .filter(testPoint => knn(trainingSet, _.initial(testPoint), k) === testPoint[3] )
         .size()
         .divide(testSetSize)
         .value()
@@ -24,8 +29,14 @@ function runAnalysis() {
 }
 
 function knn(data, point, k) {
+  // Point has 3 values
   return _.chain(data)
-  .map(row => [distance(row[0], point), row[3]])
+  .map(row => {
+    return [
+        distance(_.initial(row), point), 
+        _.last(row)
+      ]
+  })
   .sortBy(row => row[0])
   .slice(0, k)
   .countBy(row => row[1])

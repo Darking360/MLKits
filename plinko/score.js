c19871f5011074ddd1aeca857f65d3c26f76aa55
Miguel Bolivar <MBolivar360@gmail.com>
2019-01-31 16:06:33 -0400

    Adding minmax for standarization, and also modifying proram to test each feature and print accuracy

--- plinko/score.js
+++ plinko/score.js
@@ -14,18 +14,20 @@ function onScoreUpdate(dropPosition, bounciness, size, bucketLabel) {
 }
 
 function runAnalysis() {
-  const testSetSize = 10;
+  const testSetSize = 100;
+  const k = 10;
 
-    _.range(1, 15).forEach(k => {
-      const [testSet, trainingSet] = splitDataset(outputs, testSetSize, k);
-      const accuracy = _.chain(testSet)
-        .filter(testPoint => knn(trainingSet, _.initial(testPoint), k) === testPoint[3] )
-        .size()
-        .divide(testSetSize)
-        .value()
+  _.range(0, 3).forEach(feature => {
+    const data = _.map(outputs, (row) => [row[feature], _.last(row)])
+    const [testSet, trainingSet] = splitDataset(minMax(data, 1), testSetSize, k);
+    const accuracy = _.chain(testSet)
+      .filter(testPoint => knn(trainingSet, _.initial(testPoint), k) === testPoint[1] )
+      .size()
+      .divide(testSetSize)
+      .value()
 
-        console.log(`Accuracy for K = '${k}': ${accuracy}`)
-    });
+      console.log(`Accuracy for feature = '${feature}': ${parseFloat(accuracy).toFixed(4)}`)
+  });
 }
 
 function knn(data, point, k) {
@@ -52,5 +54,20 @@ function splitDataset(data, testCount) {
   const testSet = _.slice(shuffled, 0 , testCount);
   const trainingSet = _.slice(shuffled, testCount);
   return [testSet, trainingSet];
+}
+
+function minMax(data, featureCount) {
+  const clonedData = _.cloneDeep(data)
+  for (let i = 0; i < featureCount; i++) {
+    const column = clonedData.map((row) => row[i])
+
+    const min = _.min(column)
+    const max = _.max(column)
+    for (let j = 0; j < clonedData.length; j++) {
+      clonedData[j][i] = (clonedData[j][i] - min) / (max - min);
+    }
+  }
+
+  return clonedData;
 }
 

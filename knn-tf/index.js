32c1edaee0b9986fa27e96578ad29d3209b140bf
Miguel Bolivar <MBolivar360@gmail.com>
2019-02-07 17:39:15 -0400

    Adding sqft_living in order to improve the results from KNN

--- knn-tf/index.js
+++ knn-tf/index.js
@@ -5,7 +5,7 @@ const loadCSV = require('./load-csv');
 let { features, labels, testFeatures, testLabels } = loadCSV('kc_house_data.csv', {
 	shuffle: true,
 	splitTest: 10,
-	dataColumns: ['lat', 'long', 'sqft_lot'],
+	dataColumns: ['lat', 'long', 'sqft_lot', 'sqft_living'],
 	labelColumns: ['price']
 });
 

b9c599c344f0796d8b9ef57acade258013e2e14e
Miguel Bolivar <MBolivar360@gmail.com>
2019-02-21 16:12:28 -0400

    Linear regression using now tensorflow

--- regressions/linear-regression.js
+++ regressions/linear-regression.js
@@ -3,24 +3,32 @@ const _ = require('lodash')
 
 class LinearRegression {
   constructor(features, labels, options) {
-    this.features = features
-    this.labels = labels
+    this.features = tf.tensor(features)
+    this.labels = tf.tensor(labels)
 
-    this.m = 0
-    this.b = 0
+    this.features = tf.ones([ this.features.shape[0], 1 ]).concat(this.features, 1)
+
+    this.weights = tf.zeros([2,1])
 
     this.options = Object.assign({ learningRate: 0.1, iterations: 1000 }, options)
   }
-
   
-  
-  gradientDescent() {
+  /*gradientDescent() {
     const currentGuessesForMPG = this.features.map(row => (this.m * row[0]) + this.b)
     const bSlope = _.sum(currentGuessesForMPG.map((guess, index) => guess - this.labels[index][0])) * 2 / this.features.length
     const mSlope = _.sum(currentGuessesForMPG.map((guess, index) => -1 * this.features[index][0] * (this.labels[index][0] - guess))) * 2 / this.features.length
 
     this.m = this.m - mSlope * this.options.learningRate
     this.b = this.b - bSlope * this.options.learningRate
+  }*/
+
+  gradientDescent() {
+    const currentGuesses = this.features.matMul(this.weights)
+    const differences = currentGuesses.sub(this.labels)
+    const slopes = this.features.transpose()
+      .matMul(differences)
+      .div(this.features.shape[0])
+    this.weights = this.weights.sub(slopes.mul(this.options.learningRate))
   }
 
 
